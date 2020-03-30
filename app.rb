e8fb894e7883ed0976b695b57570ff2b9060abd2
Ty Bandachit <tbandachit@gmail.com>
2020-03-30 22:12:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is"
+    "My name is "
   end
   get '/hometown' do 
-    "My hometown is"
+    "My hometown is "
   end
   get '/favorite-song' do
     "My favorite song is "
